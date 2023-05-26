d505eabeb777b0b98812fb9571a97b1f09fe9385
chibuikeeugene <chibuikeeugene@gmail.com>
2023-05-26 19:44:07 +0100

    updated the template files in my dag script

--- dags/data_ingestion_gcs.py
+++ dags/data_ingestion_gcs.py
@@ -3,6 +3,7 @@
 import os
 import logging
 
+from datetime import datetime
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
@@ -14,6 +15,8 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExte
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/Users/eugene/Personal_Projects/Data_Project/app/airflow")
+
 ## ================ retrieve GCP environment variables    ===================== ##
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
@@ -21,11 +24,13 @@ BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
 
 ## ============== parameterization of variables ====================== ##
-dataset_file = "yellow_tripdata_2021-01.parquet"
+dataset_file = "yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
 dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/Users/eugene/Personal_Projects/Data_Project/app/airflow")
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
 
+
+# ================== this block of code is responsible for uploading files to google cloud storage =================== #
 def upload_to_gcs(bucket, object_name, local_file):
 
     # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
@@ -43,17 +48,19 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 default_args = {
     "owner": "airflow",
-    "start_date": days_ago(1),
+    #"start_date": datetime(2021,1,1),
     "depends_on_past": False,
     "retries": 1,
 }
 
 
 with DAG(
+    start_date=datetime(2021,1,1),
+    end_date = datetime(2021,6,1),
     dag_id="data_ingestion_gcs_dag",
-    schedule_interval="@daily",
+    schedule_interval="0 6 2 * *",
     default_args=default_args,
-    catchup=False,
+    catchup=True,
     max_active_runs=1,
     tags=['de'],
 ) as dag:
