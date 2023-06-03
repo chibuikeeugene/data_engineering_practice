a9a834a627fd41f0cfdc7539f28b8b283df0bef5
chibuikeeugene <chibuikeeugene@gmail.com>
2023-06-03 23:48:28 +0100

    added two new dags - green dags and fhv dags

--- dags/data_ingestion_gcs.py
+++ dags/data_ingestion_gcs.py
@@ -23,11 +23,23 @@ PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
 
-## ============== parameterization of variables ====================== ##
-dataset_file = "yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
-dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
+## ============== parameterization of variables for yellow taxi ====================== ##
+yellow_dataset_file = "yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
+yellow_dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{yellow_dataset_file}"
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/Users/eugene/Personal_Projects/Data_Project/app/airflow")
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+
+
+
+## ============== parameterization of variables for green taxi ====================== ##
+green_dataset_file = "green_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
+green_dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{green_dataset_file}"
+
+
+## ============== parameterization of variables for fhv ====================== ##
+fhv_dataset_file = "fhv_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
+fhv_dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{fhv_dataset_file}"
+
 
 
 # ================== this block of code is responsible for uploading files to google cloud storage =================== #
@@ -48,7 +60,6 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 default_args = {
     "owner": "airflow",
-    #"start_date": datetime(2021,1,1),
     "depends_on_past": False,
     "retries": 1,
 }
@@ -65,36 +76,110 @@ with DAG(
     tags=['de'],
 ) as dag:
     
-    download_dataset_task = BashOperator(
-        task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+    download_yellow_dataset_task = BashOperator(
+        task_id="download_yellow_dataset_task",
+        bash_command=f"curl -sSL {yellow_dataset_url} > {path_to_local_home}/{yellow_dataset_file}"
     )
 
+    download_green_dataset_task = BashOperator(
+        task_id="download_green_dataset_task",
+        bash_command=f"curl -sSL {green_dataset_url} > {path_to_local_home}/{green_dataset_file}"
+    )
+
+
+    download_fhv_dataset_task = BashOperator(
+        task_id="download_fhv_dataset_task",
+        bash_command=f"curl -sSL {fhv_dataset_url} > {path_to_local_home}/{fhv_dataset_file}"
+    )
+
+
     # TODO: research and try XCOM to communicate output values between 2 tasks/operators
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
+    local_to_gcs_task_yellow = PythonOperator(
+        task_id="local_to_gcs_task_yellow",
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{dataset_file}",
-            "local_file": f"{path_to_local_home}/{dataset_file}",
+            "object_name": f"raw/{yellow_dataset_file}",
+            "local_file": f"{path_to_local_home}/{yellow_dataset_file}",
         },
     )
 
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
+
+    local_to_gcs_task_green = PythonOperator(
+        task_id="local_to_gcs_task_green",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw2/{green_dataset_file}",
+            "local_file": f"{path_to_local_home}/{green_dataset_file}",
+        },
+    )
+
+
+
+    local_to_gcs_task_fhv = PythonOperator(
+        task_id="local_to_gcs_task_fhv",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw3/{fhv_dataset_file}",
+            "local_file": f"{path_to_local_home}/{fhv_dataset_file}",
+        },
+    )
+
+
+
+
+
+    bigquery_external_table_task_yellow = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_external_table_task_yellow",
         table_resource={
             "tableReference": {
                 "projectId": PROJECT_ID,
                 "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
+                "tableId": "yellow_table" #initially called external_table,
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{dataset_file}"],
+                "sourceUris": [f"gs://{BUCKET}/raw/{yellow_dataset_file}"],
             },
         },
     )
 
-    download_dataset_task  >> local_to_gcs_task >> bigquery_external_table_task
 
+    bigquery_external_table_task_green = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_external_table_task_green",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": "green_table",
+            },
+            "externalDataConfiguration": {
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/raw2/{green_dataset_file}"],
+            },
+        },
+    )
+
+
+    bigquery_external_table_task_fhv = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_external_table_task_fhv",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": "fhv_table",
+            },
+            "externalDataConfiguration": {
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/raw3/{fhv_dataset_file}"],
+            },
+        },
+    )
+
+
+
+    download_yellow_dataset_task  >> local_to_gcs_task_yellow >> bigquery_external_table_task_yellow
+    download_green_dataset_task  >> local_to_gcs_task_green >> bigquery_external_table_task_green
+    download_fhv_dataset_task  >> local_to_gcs_task_fhv >> bigquery_external_table_task_fhv
